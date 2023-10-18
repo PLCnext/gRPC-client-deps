11d2a6d09b41c531ab3e1f13c710272310924b73
Hermann Spies <hspies@phoenixcontact.com>
2023-10-18 08:58:46 +0200

    Update c-ares-targets-noconfig.cmake

--- plcnext_deps/axcf2_deps/cmake_modules/ros/c-ares/c-ares-targets-noconfig.cmake
+++ plcnext_deps/axcf2_deps/cmake_modules/ros/c-ares/c-ares-targets-noconfig.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "c-ares::cares" for configuration ""
 set_property(TARGET c-ares::cares APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
 set_target_properties(c-ares::cares PROPERTIES
-  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libcares.so.2.4.0"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libcares.so.2.5.1"
   IMPORTED_SONAME_NOCONFIG "libcares.so.2"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS c-ares::cares )
-list(APPEND _IMPORT_CHECK_FILES_FOR_c-ares::cares "${_IMPORT_PREFIX}/lib/libcares.so.2.4.0" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_c-ares::cares "${_IMPORT_PREFIX}/lib/libcares.so.2.5.1" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
